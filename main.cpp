0e7d90c43f885572f29de4379ac99fb2ea2ee001
Jakub Ogrodnik <81594386+JakubOgrodnikPL@users.noreply.github.com>
2022-08-29 20:17:36 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main()
+{
+    cout << "Using elements from standard namespace without 'using namespace std;'" << endl;
+    return 0;
+}
